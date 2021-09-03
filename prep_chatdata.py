4209589b4b3425328e9387778b21cbbe83ec9e02
juholeinonen <juho.leinonen@aalto.fi>
2021-09-03 14:04:22 +0300

    fit to opensubtitles, argparse wrappings

--- prep_chatdata.py
+++ prep_chatdata.py
@@ -8,27 +8,35 @@ import sys
 import argparse
 import morfessor
 
+def parse_arguments():
+    parser = argparse.ArgumentParser(description='Preparing corpus for Transformer Language Model')
+    parser.add_argument('--split_type', type=str, default='word',
+                        choices=['word', '+morf+', '+char+', 'char'],
+                        help='How to split the corpus')
+    parser.add_argument('dataset', type=str,
+                        help='The dataset to prepare')
+    parser.add_argument('--morfessor_model', type=str,
+                        help='The morfessor model')
+    parser.add_argument('--clean_string', action='store_true',
+                        help='Lowers text, uses regex command to clean.')
+    parser.add_argument('--chat_format', type=str, default='convai2',
+                        choices=['convai2'],
+                        help='In which format is the chat saved to text.')
+    args = parser.parse_args()
 
-parser = argparse.ArgumentParser(description='Preparing corpus for Transformer Language Model')
-parser.add_argument('--split_type', type=str, default='word',
-                    choices=['word', '+morf+', '+char+', 'char'],
-                    help='How to split the corpus')
-parser.add_argument('dataset', type=str,
-                    help='The dataset to prepare')
-parser.add_argument('--morfessor_model', type=str,
-                    help='The morfessor model')
-args = parser.parse_args()
+    morfessor_binary = None
+    if args.split_type == '+morf+':
+        if args.morfessor_model is None:
+            parser.error("option +morphs+ requires a matching Morfessor model.")
+        else:
+            morfessorIO = morfessor.MorfessorIO()
+            morfessor_binary = morfessorIO.read_binary_model_file(args.morfessor_model)
 
-if args.split_type == '+morf+':
-    if args.morfessor_model is None:
-        parser.error("option +morphs+ requires a matching Morfessor model.")
-    else:
-        morfessorIO = morfessor.MorfessorIO()
-        MorfessorModel = morfessorIO.read_binary_model_file(args.morfessor_model)
+    return args, morfessor_binary 
 
 
-def splitText(line):
-    if args.split_type == '+char+':
+def split_text(line, split_type, morfessor_binary):
+    if split_type == '+char+':
         sentenceAsCharacters = []
         words = line.split()
         for word in words:
@@ -41,11 +49,11 @@ def splitText(line):
                 sentenceAsCharacters.append("+" + word[-1])
         line = " ".join(sentenceAsCharacters)
 
-    elif args.split_type == '+morf+':
+    elif split_type == '+morf+':
         sentenceAsMorfs = []
         words = line.split()
         for word in words:
-            morfs, _ = MorfessorModel.viterbi_segment(word)
+            morfs, _ = morfessor_binary.viterbi_segment(word)
             if len(morfs) == 1:
                 sentenceAsMorfs.append(morfs[0])
             else:
@@ -57,35 +65,55 @@ def splitText(line):
     return line
 
 
-def cleanText():
-    trainFileName, file_ext = os.path.splitext(args.dataset)
+def prepare_chat(arguments, morfessor_binary):
+    trainFileName, file_ext = os.path.splitext(arguments.dataset)
 
-    if args.split_type == '+char+':
+    if arguments.split_type == '+char+':
         varyingFileName = "pCharp"
-    elif args.split_type == '+morf+':
+    elif arguments.split_type == '+morf+':
         varyingFileName = "pMorfp"
     else:
         varyingFileName = "ERROR"
 
     cleanedFileName = trainFileName + "_cleaned" + varyingFileName + file_ext
 
-    with open(args.dataset, 'r', encoding='utf-8') as f_source,\
-            open(cleanedFileName, 'w', encoding='utf-8') as f_target:
+    with open(arguments.dataset, 'r', encoding='utf-8') as f_source,\
+        open(cleanedFileName, 'w', encoding='utf-8') as f_target:
         for line in f_source:
-            line_parts = line.split(' ', maxsplit=1)
-            line = line_parts[1]
-            line = line.lower()
-            # I Don't remember what this does.. removes certain characters?
-            line = re.sub("[^a-zA-Z\x7f-\xff]", " ", line)
-            line = re.sub(" +", " ", line)
-            line = line.strip()
-            if len(line) == 0:
-                line = "<UNK>"
-                f_target.write(line_parts[0] + ',' + line + "\n")
-                continue
+            if arguments.chat_format == "convai2":
+                # Splits the number of Q/A and the text itself
+                line_parts = line.split(' ', maxsplit=1)
+                dialog_round_number =   line_parts[0]
+                dialog_round_text   =   line_parts[1]
 
-            line = splitText(line)
+                dialog_round_in_subwords = []
 
-            f_target.write(line_parts[0] + ',' + line + "\n")
+                dialog_turns = dialog_round_text.split('\t', maxsplit=1) # does not work now if sentence_part has the false choices
+            else:
+                # placeholder, currently everything should be convai2
+                dialog_turns = line.split()
 
-cleanText()
+            for dialog_turn in dialog_turns:
+                if arguments.clean_string:
+                    dialog_turn = dialog_turn.lower()
+                    # I Don't remember what this does.. removes certain characters?
+                    dialog_turn = re.sub("[^a-zA-Z\x7f-\xff]", " ", dialog_turn)
+                    dialog_turn = re.sub(" +", " ", dialog_turn)
+
+                dialog_turn = dialog_turn.strip()
+                if len(dialog_turn) == 0:
+                    dialog_round_in_subwords.append('<UNK>')
+                else:
+                    dialog_round_in_subwords.append(split_text(dialog_turn, arguments.split_type, morfessor_binary))
+
+            dialog_to_write = "\t".join(dialog_round_in_subwords)
+            f_target.write(dialog_round_number + ' ' + dialog_to_write + '\n')
+
+
+def main(arguments):
+    prepare_chat(arguments, morfessor_binary)
+
+
+if __name__ == '__main__':
+    arguments, morfessor_binary = parse_arguments()
+    main(arguments, morfessor_binary)
